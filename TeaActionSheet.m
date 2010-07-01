62d480eeb8df70c6b5e47f713ab52211741b6d0c
Cameron Desautels <camdez@gmail.com>
2010-07-01 03:06:51 -0500

    Cleaned up TeaActionSheet.m, commented

--- TeaActionSheet.m
+++ TeaActionSheet.m
@@ -13,50 +13,58 @@
 
 - (id)initWithTitle:(NSString *)title delegate:(id<UIActionSheetDelegate>)delegate cancelButtonTitle:(NSString *)cancelButtonTitle destructiveButtonTitle:(NSString *)destructiveButtonTitle otherButtonTitles:(NSString *)otherButtonTitles, ...
 {
-    const unsigned int max_args = 8; // TODO see how many will fit on screen and raise this number
-    unsigned int arg_count = 0;
-    NSString* argsa[max_args]; // XXX I could use an NSMutableArray but I can only pass a fixed number of arguments in the final call anyway
-
-    va_list args;
-    va_start(args, otherButtonTitles);
-
     /*
-    while ((argsa[arg_count] = va_arg(args, id)) && (arg_count < max_args)) {
-        arg_count++;
-    }
-     */
+     XXX: This implementation is a bit odd, but there's currently no way around it.  Because the only
+     initializer for UIActionSheet is variadic, and because there's no way in Objective-C (/C) to unpack the
+     variadic arguments we receive in order to pass them on to the [super init:...] call, it's impossible to
+     subclass UIActionSheet properly.  In fact, it's impossible to properly subclass any class with methods
+     taking variadic arguments unless we are provided with a non-variadic version of the method that we can
+     call (generally either a version taking an NSArray* or va_list).  Given this sitation, the best we can do
+     is to provide a version of the method that works with a fixed maximum number of arguments.
 
-    for (NSString *arg = otherButtonTitles; (arg != nil) && (arg_count < max_args); arg = va_arg(args, NSString*))
-    {
-        argsa[arg_count] = arg;
-        arg_count++;
-    }
+     While this sounds awful, for this particular class things aren't so bad--the variadic arguments represent
+     buttons to be displayed on the screen, and since only so many buttons will fit on the screen, there's a
+     built in maximum...basically.  The maximum number of buttons that will fit on the screen turns out to be
+     six, so that's the maximum enforced here.  Actually, however, UIActionSheet is willing to take an
+     unbounded number of button titles, which it then puts into a UITableView (interesting trivia, eh?).  But
+     considering that adding that many buttons is a horrible UI decision, I'm leaving the cap at six.
+    */
 
+    // Unpack the variadic arguments
+    const unsigned int maxArgs = 6;
+    NSString* args[maxArgs];
+
+    va_list packedArgs;
+    va_start(packedArgs, otherButtonTitles);
+
+    unsigned int argCount = 0;
+
+    for (NSString *arg = otherButtonTitles; (arg != nil) && (argCount < maxArgs); arg = va_arg(packedArgs, NSString*))
+        args[argCount++] = arg;
+
+    va_end(packedArgs);
+
+    // Initialize, passing the otherButtonTitles we got
     self = [super initWithTitle:title
                        delegate:delegate
               cancelButtonTitle:cancelButtonTitle
          destructiveButtonTitle:destructiveButtonTitle
-              otherButtonTitles:(arg_count > 0 ? argsa[0] : nil),
-                                (arg_count > 1 ? argsa[1] : nil),
-                                (arg_count > 2 ? argsa[2] : nil),
-                                (arg_count > 3 ? argsa[3] : nil),
-                                (arg_count > 4 ? argsa[4] : nil),
-                                (arg_count > 5 ? argsa[5] : nil),
-                                (arg_count > 6 ? argsa[6] : nil),
-                                (arg_count > 7 ? argsa[7] : nil),
+              otherButtonTitles:(argCount > 0 ? args[0] : nil),
+                                (argCount > 1 ? args[1] : nil),
+                                (argCount > 2 ? args[2] : nil),
+                                (argCount > 3 ? args[3] : nil),
+                                (argCount > 4 ? args[4] : nil),
+                                (argCount > 5 ? args[5] : nil),
                                 nil];
 
-    va_end(args);
-
-    // Register for notification
-    [[NSNotificationCenter defaultCenter] addObserverForName:UIApplicationDidEnterBackgroundNotification
-                                                      object:nil
-                                                       queue:nil
-                                                  usingBlock:^(NSNotification *notification){
-                                                      [self dismissWithClickedButtonIndex:self.cancelButtonIndex animated:NO];
-                                                  }];
-
-    // TODO should probably do a check to see if the [super init...] call succeeded
+    if (self) {
+        [[NSNotificationCenter defaultCenter] addObserverForName:UIApplicationDidEnterBackgroundNotification
+                                                          object:nil
+                                                           queue:nil
+                                                      usingBlock:^(NSNotification *notification){
+                                                          [self dismissWithClickedButtonIndex:self.cancelButtonIndex animated:NO];
+                                                      }];
+    }
     return self;
 }
 
